9f2a3495e000dcf1e246bbc4629d8fce2b2fb524
HugoBzn <hugobazan1499@gmail.com>
2022-08-24 15:00:33 -0500

    Navigation Component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
-<script setup>
-
+<script>
+  // Importando el componente de navegacion
+  import TheNavigation from '@/components/TheNavigation.vue';
+  export default {
+    components: {
+      TheNavigation
+    }
+  }
 </script>
 
 <template>
-<div id="nav">
-  <router-link to="/">Inicio</router-link>
-  <router-link to="/brazil">Brazil</router-link>
-  <router-link to="/hawaii">Hawaii</router-link>
-  <router-link to="/panama">Panama</router-link>
-  <router-link to="/jamaica">Jamaica</router-link>
-</div>
+  <the-navigation></the-navigation>
   <div class="container">
-    <router-view></router-view>
+      <router-view></router-view>
   </div>
 </template>
