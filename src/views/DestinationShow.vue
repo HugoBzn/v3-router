25e6742a5c155392db1a508dbae53bd8adfa1fbd
HugoBzn <hugobazan1499@gmail.com>
2022-08-30 13:31:39 -0500

    Experience cards

--- src/views/DestinationShow.vue
+++ src/views/DestinationShow.vue
@@ -1,36 +1,49 @@
 <script>
 // Importando datos
-import sourceData from '@/data.json'
+import sourceData from "@/data.json";
+// Importando componente
+import ExperienceCard from "@/components/ExperienceCard.vue";
 
 export default {
+  components: {
+    ExperienceCard,
+  },
   props: {
     id: {
       type: Number,
-      required: true
-    }
+      required: true,
+    },
   },
   computed: {
     destination() {
-      return sourceData.destinations.find(destination => {
-        return destination.id === this.id })
-    }
-  }
-}
+      return sourceData.destinations.find((destination) => {
+        return destination.id === this.id;
+      });
+    },
+  },
+};
 </script>
 
 <template>
-    <section class="destination">
-        <h1>{{ destination.name }}</h1>
-        <div class="destination-details">
-            <img 
-            :src="`/images/${destination.image}`"
-            :alt="destination.name">
-            <p>
-                {{ destination.description }}
-            </p>
-        </div>
-    </section>
+  <section class="destination">
+    <h1>{{ destination.name }}</h1>
+    <div class="destination-details">
+      <img :src="`/images/${destination.image}`" :alt="destination.name" />
+      <p>
+        {{ destination.description }}
+      </p>
+    </div>
+  </section>
+  <section class="experiences">
+    <h2>Mejores experiencias en {{ destination.name }}</h2>
+    <div class="cards">
+      <experience-card
+      v-for="experience in destination.experiences"
+      :key="experience.slug"
+      :experience="experience"
+      />
+    </div>
+  </section>
 </template>
 
-<style lang="scss" scoped>
-</style>
+<style lang="scss" scoped></style>
